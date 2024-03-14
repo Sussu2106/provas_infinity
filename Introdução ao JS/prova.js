8cdedd52cad4bc86ec5f923846dffb3730b3d184
Sussu2106 <116111242+Sussu2106@users.noreply.github.com>
2024-03-13 21:13:38 -0300

    Add files via upload

---
+++ Introdução ao JS/prova.js
@@ -0,0 +1,13 @@
+
+const n1 = 8;
+const n2 = 7;
+const n3 = 9; 
+const p1 = 0.3;  
+const p2 = 0.4; 
+const p3 = 0.3;  
+
+
+const media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+
+
+console.log("A média ponderada é: " + media);

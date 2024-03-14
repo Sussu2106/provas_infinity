0dcadc16d5ad7951e40a8ebec41718d73edd192e
Sussu2106 <116111242+Sussu2106@users.noreply.github.com>
2024-03-13 21:17:12 -0300

    Update prova.js

--- Introdução ao JS/prova.js
+++ Introdução ao JS/prova.js
@@ -1,13 +1,13 @@
 
-const n1 = 8;
-const n2 = 7;
-const n3 = 9; 
-const p1 = 0.3;  
-const p2 = 0.4; 
-const p3 = 0.3;  
+var n1 = 8;
+var n2 = 7;
+var n3 = 9; 
+var p1 = 0.3;  
+var p2 = 0.4; 
+var p3 = 0.3;  
 
 
-const media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+var media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
 
 
 console.log("A média ponderada é: " + media);
